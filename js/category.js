9b723b371e4b5b9c333fdb714e21e5949104f594
gzczwyl20 <wyl123lili@163.com>
2018-08-10 21:18:08 +0800

    分类完成

--- js/category.js
+++ js/category.js
@@ -1,25 +1,68 @@
-$(function(){
+$(function () {
   init();
+
   function init() {
     setHTML();
-    var leftscroll = new IScroll('.left');
+    getCategories(function (res) {
+      // console.log(res);
+      var leftHtml = template('left', {
+        data: res.data
+      });
+      $('.view_title').html(leftHtml);
+      var leftscroll = new IScroll('.left');
+    });
+    getCategories(function (res) {
+      // console.log(res.data[0].children);
+
+      var rightHtml = template('right', {
+        data: res.data[0].children
+      })
+      $('.right_box').html(rightHtml);
+      var rightscroll = new IScroll('.right');
+    })
   }
 
-  function setHTML(){
+  function setHTML() {
     // 基础值
     var baseVal = 100;
     // 设计稿的宽度
-    var pageWidth =375;
+    var pageWidth = 375;
     // 当前屏幕的宽度
     var scrollWidth = document.querySelector('html').offsetWidth;
     // 要设置的的fontsize
-    var fz = scrollWidth*baseVal / pageWidth;
+    var fz = scrollWidth * baseVal / pageWidth;
     // 赋值给html
     // console.log(fz);
-    
-    document.querySelector('html').style.fontSize=fz+'px';
+
+    document.querySelector('html').style.fontSize = fz + 'px';
   }
-  window.onresize=function(){
+  window.onresize = function () {
     setHTML();
   }
+
+  //发送请求 获取数据
+  function getCategories(callback) {
+    $.get('categories', function (res) {
+      callback && callback(res);
+    }, 'json')
+  }
+
+  // 点击按钮渲染页面
+  $('.view_title').on('tap','li',function(){
+    // console.log(123);
+    var val = this.dataset.val;
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    // console.log(val);
+    getCategories(function (res) {
+      // console.log(res.data[0].children);
+
+      var rightHtml = template('right', {
+        data: res.data[val].children
+      })
+      $('.right_box').html(rightHtml);
+      var rightscroll = new IScroll('.right');
+    })
+    
+  })
 })
